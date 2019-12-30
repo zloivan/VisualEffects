b14dfa483765b2979923a2e80db00a9ab448ca18
ivankhomichkov <ivan.khomichkov@iridescent.studio>
2019-12-30 17:50:24 +0300

    add initial area of effect

--- Assets/EffectController.cs
+++ Assets/EffectController.cs
@@ -10,6 +10,7 @@ public class EffectController : MonoBehaviour
     [SerializeField] private Button buttonPrefab;
     [SerializeField] private RectTransform rootForButtons;
 
+    [SerializeField] private ParticleSystem[] group;
     private Button[] listOfButtons;
 
     void Start()
@@ -20,29 +21,47 @@ public class EffectController : MonoBehaviour
     private void InstantiateButtons()
     {
         var numberOfParticleSystem = particleSystems.Length;
-        listOfButtons = new Button[numberOfParticleSystem];
 
-
-        for (var i = 0; i < numberOfParticleSystem; i++)
+        if (numberOfParticleSystem != 0)
         {
-            var ps = particleSystems[i];
-            var newButton = Instantiate(buttonPrefab, rootForButtons);
-            newButton.GetComponentInChildren<Text>().text = particleSystems[i].gameObject.name;
+            listOfButtons = new Button[numberOfParticleSystem];
+            for (var i = 0; i < numberOfParticleSystem; i++)
+            {
+                var ps = particleSystems[i];
+                var newButton = Instantiate(buttonPrefab, rootForButtons);
+                newButton.GetComponentInChildren<Text>().text = particleSystems[i].gameObject.name;
             
-            newButton.onClick.AddListener(() =>
-                {
-                    if (ps.isEmitting)
+                newButton.onClick.AddListener(() =>
                     {
-                        ps.Stop();
-                    }
-                    else
-                    {
-                        ps.Play(); 
-                    }
+                        if (ps.isEmitting)
+                        {
+                            ps.Stop();
+                        }
+                        else
+                        {
+                            ps.Play(); 
+                        }
                     
+                    }
+                );
+                listOfButtons[i] = newButton; 
+            }
+        }
+
+        var lengthOfGrou = group.Length;
+        if (group != null && lengthOfGrou != 0)
+        {
+            var groupButton = Instantiate(buttonPrefab, rootForButtons);
+            groupButton.GetComponentInChildren<Text>().text = "Group";
+            groupButton.onClick.AddListener(() =>
+                {
+                    for (int i = 0; i < lengthOfGrou; i++)
+                    {
+                        group[i].Play();
+                    }
                 }
             );
-            listOfButtons[i] = newButton; 
+            
         }
     }
 }
